3a8919752a97fe475d212f4e1c64d2d79cd283e0
mark-joyce <markjoyce25ps@gmail.com>
2020-02-10 12:00:07 +0000

    Changed Site Title, Added Min Height in CSS, and Numbered Markers in the Map

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# 'The Digital Nomad Top 10' Interactive World Map Site
+# 'The Digital Nomad 9' Interactive World Map Site
 
 Milestone Two Project: Interactive Frontend Development - Code Institute
 
 This is an Interactive site that calls on an external API to show the best digital nomad destinations around the world with information & links to go with it.
-The site has a map that markers 10 numbered destinations around the world, best for aspiring or veteran digital nomads to work & live from.
+The site has a map that marks the best destinations around the world, best for aspiring or veteran digital nomads to work & live from.
 When a marker location is clicked - all helpful information, details & useful links show on the side panel to the left of the map.
 
 ## Live Website
@@ -35,7 +35,6 @@ The live website for the agency can be found [here](https://mark-joyce.github.io
 3. Bootstrap
 4. JQuery
 5. Javascript
-6.
 
 ## Features
 
@@ -45,6 +44,11 @@ The live website for the agency can be found [here](https://mark-joyce.github.io
 
 ## Testing
 
+### Planning
+
+### Implementation
+
+### Results & Outcomes
 
 ## Deployment
 
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,94 +1,125 @@
-// Initialize and add the map
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 3,
-    center: new google.maps.LatLng(0, 0),
-    mapTypeId: 'terrain',
-    gestureHandling: 'greedy'
+var labels = '123456789';
+var labelIndex = 0;
+
+function initialize() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    gestureHandling: 'greedy',
+    disableDefaultUI: true,
+    center: {
+      lat: 0,
+      lng: 0
+    }
   });
 
-  // The location of Top 10 Destinations
+  // Canggu No.1
   var canggu = {
     lat: -8.650374779810114,
     lng: 115.13611793518066
   };
+
+  var marker = new google.maps.Marker({
+    position: canggu,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Chiang Mai No.2
   var chiangmai = {
     lat: 18.788911265422144,
     lng: 98.98707389831543
   };
+
+  var marker = new google.maps.Marker({
+    position: chiangmai,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Buenos Aires No.3
   var buenosaires = {
     lat: -34.607568127801215,
     lng: -58.43756675720214
   };
+
+  var marker = new google.maps.Marker({
+    position: buenosaires,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Prague No.4
   var prague = {
     lat: 50.087409586962714,
     lng: 14.421272277832031
   };
+
+  var marker = new google.maps.Marker({
+    position: prague,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Medellin No.5
   var medellin = {
     lat: 6.24440122931306,
     lng: -75.57297706604004
   };
+
+  var marker = new google.maps.Marker({
+    position: medellin,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Lisbon No.6
   var lisbon = {
     lat: 38.708218629351634,
     lng: -9.136676788330078
   };
+
+  var marker = new google.maps.Marker({
+    position: lisbon,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Budapest No.7
   var budapest = {
     lat: 47.498473895818734,
     lng: 19.039735794067383
   };
+
+  var marker = new google.maps.Marker({
+    position: budapest,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Kuala Lumpur No.8
   var hochiminhcity = {
     lat: 10.775638537653744,
     lng: 106.7021369934082
   };
+
+  var marker = new google.maps.Marker({
+    position: hochiminhcity,
+    map: map,
+    label: labels[labelIndex++ % labels.length]
+  });
+
+  // Kuala Lumpur No.9
   var kualalumpur = {
     lat: 3.1569707170633405,
     lng: 101.70086860656738
   };
-  var mexicocity = {
-    lat: 19.432600481483334,
-    lng: -99.13328647613525
-  };
 
-
-  // The markers
   var marker = new google.maps.Marker({
-    position: canggu,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: chiangmai,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: buenosaires,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: prague,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: medellin,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: lisbon,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: budapest,
-    map: map
-  });
-    var marker = new google.maps.Marker({
-    position: hochiminhcity,
-    map: map
-  });
-    var marker = new google.maps.Marker({
     position: kualalumpur,
-    map: map
+    map: map,
+    label: labels[labelIndex++ % labels.length]
   });
-    var marker = new google.maps.Marker({
-    position: mexicocity,
-    map: map
-  });
+
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
